8d1d0ee8cfef66b4aed34c012c7616151d814d26
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-08 10:00:06 +0800

    - initialized project

---
+++ Exploring-MapKit/View/Main Views/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Exploring-MapKit
+//
+//  Created by Ronjie Diafante Man-on on 5/8/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
