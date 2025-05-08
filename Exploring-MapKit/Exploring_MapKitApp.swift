36fa1f294356dee753a2c94fc29df9708176c2ab
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-08 10:00:24 +0800

    - initialized project

---
+++ Exploring-MapKit/Exploring_MapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exploring_MapKitApp.swift
+//  Exploring-MapKit
+//
+//  Created by Ronjie Diafante Man-on on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Exploring_MapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
